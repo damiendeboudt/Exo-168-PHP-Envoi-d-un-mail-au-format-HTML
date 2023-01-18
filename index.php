58e4697d465400f5bef7a41c7edf46e267d7f4eb
Damien <damien.deboudt@sfr.fr>
2023-01-18 16:30:52 +0100

    ok

--- index.php
+++ index.php
@@ -3,13 +3,14 @@
  * 1. Complétez la variable $html, elle devra contenir un texte court de newsletter.
  * 2. Ajoutez au moins une adresse mail ( la vôtre ? ) au tableau $to.
  * 3. Envoyez ce mail au format HTML à toutes les personnes de $ to qui ont souscrit à votre newsletter.
- * Bonus, pour chaque adresse mail indiquer si le mail a bien été envoyer, ou faire un tableau contenant les succès / erreurs et l'afficher après envoi
+ * Bonus, pour chaque adresse mail indiquer si le mail a bien été envoyer, ou faire un tableau contenant les succès
+ * / erreurs et l'afficher après envoi
  *    ex: ['j.conan@fondationface.org' => true, 'toto@fondationface.org' => false] => à vous de trouver la suite !
  */
 
 $to = [
     'j.conan@fondationface.org',
-    ...
+    'damien.deboudt@sfr.fr'
 ];
 
 $html = '
@@ -19,9 +20,17 @@ $html = '
         </head>
         <body>
             <div>
-                <!-- Vos actualités ici -->
+                <span>Information importante sur les nouveaux arrivages</span>
             </div>
         </body>
     </html>
 ';
+for($i = 0; $i < count($to); $i++) {
+    mail($i, "Newsletter", $html);
+    if ( mail($i, "Newsletter", $html)) {
+        echo "le mail a bien été envoyé à " . $i;
+    } else {
+        echo "Erreur lors de l'envoi du mail à " . $i;
+    }
 
+}
